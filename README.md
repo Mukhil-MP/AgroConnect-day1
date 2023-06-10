67f8c09274f9263ce95d7268a597f16349910e59
Shuaib Meera <shuaibsmeera0101@gmail.com>
2023-06-10 15:37:36 +0530

    initai

---
+++ README.md
@@ -0,0 +1,3 @@
+# agroconnect_day1
+
+A new Flutter project.
---
+++ lib/screens/start.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Start extends StatelessWidget {
+  const Start({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color(0xff05FF00), Color(0xff000000)]),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(left: 21, right: 21),
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset("assets/images/agroconnect_text.png"),
+          ),
+          Container(
+            alignment: Alignment.bottomCenter,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset('assets/images/pashu.png'),
+          )
+        ],
+      ),
+    );
+  }
+}
